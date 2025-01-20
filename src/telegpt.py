ea0439e015e016d217752aaaa1dd981999652453
Nick Mazurkin <mazurkin@gmail.com>
2025-01-19 20:05:36 -0500

    update

--- src/telegpt.py
+++ src/telegpt.py
@@ -10,6 +10,7 @@ import tzlocal
 import yaml
 import telethon
 import datetime
+import asyncio
 
 import ollama
 
@@ -54,14 +55,14 @@ class TeleGptApplication:
 
     @argh.arg('--app-id', type=str, required=False, help='application identifier')
     @argh.arg('--app-hash', type=str, required=False, help='application hash')
-    @argh.arg('--app-phone', type=str, required=False, help='application phone')
+    @argh.arg('--phone', type=str, required=False, help='the phone number of the Telegram account')
     @argh.arg('--prompt', type=str, required=False, help='the name of the prompt file')
     @argh.arg('--chat', type=str, required=False, help='chat name')
-    @argh.arg('--date', type=str, required=False, help='chat date')
+    @argh.arg('--date', type=str, required=False, help='chat date (2025-01-01)')
     def main(self,
              app_id: t.Optional[int] = None,
              app_hash: t.Optional[str] = None,
-             app_phone: t.Optional[str] = None,
+             phone: t.Optional[str] = None,
              chat: t.Optional[str] = None,
              date: t.Optional[str] = None,
              prompt: str = 'example.txt',
@@ -78,8 +79,8 @@ class TeleGptApplication:
         if not app_hash:
             app_hash = os.environ['TELEGPT_APP_HASH']
 
-        if not app_phone:
-            app_phone = os.environ['TELEGPT_PHONE']
+        if not phone:
+            phone = os.environ['TELEGPT_PHONE']
 
         if not chat:
             chat = os.environ['TELEGPT_CHAT']
@@ -87,91 +88,102 @@ class TeleGptApplication:
         if not date:
             date = datetime.datetime.now(tz=self.TIMEZONE).strftime('%Y-%m-%d')
 
-        conversation: t.List[str] = self.fetch(app_id, app_hash, app_phone, chat, date)
+        # fetch the conversation
+        conversation: t.List[str] = self.fetch_conversation(app_id, app_hash, phone, chat, date)
 
-        response = self.contextualize(prompt, conversation)
+        # summarize the conversation
+        response = self.summarize_conversation(prompt, conversation)
+
+        # log the conversation
         logging.info('TELEGPT(chat: "%s", day: "%s"):\n\n%s', chat, date, response)
 
-    def fetch(self, app_id: int, app_hash: str, app_phone: str, chat: str, date: str) -> t.List[str]:
+    def fetch_conversation(self, app_id: int, app_hash: str, phone: str, chat: str, date: str) -> t.List[str]:
         client = telethon.TelegramClient(
             session=str(self.session_path.resolve()),
             api_id=app_id,
             api_hash=app_hash,
         )
 
+        with client:
+            coroutine: t.Coroutine = self.fetch_conversation_async(client, phone, chat, date)
+            return client.loop.run_until_complete(coroutine)
+
+    async def fetch_conversation_async(self,
+                                       client: telethon.TelegramClient,
+                                       phone: str,
+                                       chat: str,
+                                       date: str,
+                                       ) -> t.List[str]:
+        await client.connect()
+
+        if not await client.is_user_authorized():
+            await client.send_code_request(phone)
+            await client.sign_in(phone, input('Enter the code: '))
+
+        profile = await client.get_me()
+        logging.info('Logged in as [%s %s %s]', profile.first_name, profile.last_name, profile.phone)
+
+        # fetch all dialogs
+        chat_id: t.Optional[int] = None
+
+        async for dialog in client.iter_dialogs():
+            logging.debug('dialog [%s]=%s', dialog.name, dialog.id)
+
+            if dialog.name == chat:
+                chat_id = dialog.id
+
+        if not chat_id:
+            raise ValueError('Chat is not found: ' + chat)
+
+        # define the day
+        date_offset = datetime.datetime.strptime(date, '%Y-%m-%d')
+        date_offset = self.TIMEZONE.localize(date_offset)
+
+        # fetch all messages
+        messages: t.AsyncIterator = client.iter_messages(
+            entity=chat_id,
+            limit=self.MESSAGE_LIMIT,
+            offset_date=date_offset,
+            reverse=True,
+        )
+
         conversation: t.List[str] = []
 
-        async def process():
-            await client.connect()
+        async for message in messages:
+            if not message.text:
+                continue
+            if 'TELEGPT' in message.text:
+                continue
 
-            if not await client.is_user_authorized():
-                await client.send_code_request(app_phone)
-                await client.sign_in(app_phone, input('Enter the code: '))
+            message_text = message.text.replace('\n', ' ').replace('\r', '')
 
-            profile = await client.get_me()
-            logging.info('Logged in as [%s %s %s]', profile.first_name, profile.last_name, profile.phone)
+            message_date: datetime.datetime = message.date.astimezone(self.TIMEZONE)
+            if message_date.date() != date_offset.date():
+                break
 
-            # fetch all dialogs
-            chat_id: t.Optional[int] = None
-            async for dialog in client.iter_dialogs():
-                logging.debug('dialog "%s"=%s', dialog.name, dialog.id)
+            message_author: str
+            if message.sender.first_name and message.sender.last_name:
+                message_author = message.sender.first_name + ' ' + message.sender.last_name
+            elif message.sender.first_name:
+                message_author = message.sender.first_name
+            elif message.sender.last_name:
+                message_author = message.sender.last_name
+            else:
+                message_author = str(message.sender.id)
 
-                if dialog.name == chat:
-                    chat_id = dialog.id
-
-            if not chat_id:
-                raise ValueError('Chat is not found: ' + chat)
-
-            # define the day
-            date_offset = datetime.datetime.strptime(date, '%Y-%m-%d')
-            date_offset = self.TIMEZONE.localize(date_offset)
-
-            # fetch all messages
-            messages = client.iter_messages(
-                entity=chat_id,
-                limit=self.MESSAGE_LIMIT,
-                offset_date=date_offset,
-                reverse=True,
+            logging.debug(
+                '%s, %s: [%s] %s',
+                message.id,
+                message_date.strftime('%Y-%m-%d %H:%M:%S'),
+                message_author,
+                message_text,
             )
 
-            async for message in messages:
-                if not message.text:
-                    continue
-                if 'TELEGPT' in message.text:
-                    continue
-
-                message_text = message.text.replace('\n', ' ').replace('\r', '')
-
-                message_date: datetime.datetime = message.date.astimezone(self.TIMEZONE)
-                if message_date.date() != date_offset.date():
-                    break
-
-                message_author: str
-                if message.sender.first_name and message.sender.last_name:
-                    message_author = message.sender.first_name + ' ' + message.sender.last_name
-                elif message.sender.first_name:
-                    message_author = message.sender.first_name
-                elif message.sender.last_name:
-                    message_author = message.sender.last_name
-                else:
-                    message_author = str(message.sender.id)
-
-                logging.debug(
-                    '%s, %s: [%s] %s',
-                    message.id,
-                    message_date.strftime('%Y-%m-%d %H:%M:%S'),
-                    message_author,
-                    message_text,
-                )
-
-                conversation.append(f'"{message_author}": {message_text}')
-
-        with client:
-            client.loop.run_until_complete(process())
+            conversation.append(f'"{message_author}": {message_text}')
 
         return conversation
 
-    def contextualize(self, prompt_file: str, conversation: t.List[str]) -> str:
+    def summarize_conversation(self, prompt_file: str, conversation: t.List[str]) -> str:
         system = """
         You are the expert who analyses conversation between multiple friends.
         You will be questioned with questions. You have to answer every question in the most detailed way quoting
@@ -188,7 +200,7 @@ class TeleGptApplication:
         query = prompt_text.format(content=content)
 
         options: t.Dict = {
-            'temperature': 0.1,
+            'temperature': 0.01,
         }
 
         response: ollama.ChatResponse = ollama.generate(
